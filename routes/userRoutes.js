6e46ae45c1af436a9033d4a1663b6738dc5c32ea
ThaminduChankana <thaminduchankana@gmail.com>
2024-04-09 01:22:15 +0530

    added test cases

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser,authUser, getUser, updateUser, deleteUser } = require('../controllers/userControllers');
+const { registerUser,authUser, getUser, updateUser, deleteUser } = require('../controllers/userController');
 const router = express.Router()
 const { protect } = require("../middlewares/authMiddleware");
 
