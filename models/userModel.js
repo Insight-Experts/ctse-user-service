2f6e8c6229feea13a013738e8625313c94badb96
ThaminduChankana <thaminduchankana@gmail.com>
2024-04-16 18:49:16 +0530

    updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -16,6 +16,10 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    nic: {
+      type: String,
+      required: true,
+    },
     isAdmin: {
       type: Boolean,
       required: true,
